7785675b6e34c1bb5bc936de8485836eef214937
Antonio Servetti <antonio.servetti@polito.it>
2019-05-10 11:37:38 +0200

    Angular framework - mock service

--- angular-framework/src/app/app.component.ts
+++ angular-framework/src/app/app.component.ts
@@ -1,24 +1,36 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  cur = 0;
-  title: string = 'Tour of Heroes';
-  heroes = [
-    { str: 'Windstorm' }, 
-    { str: 'Bombasto' }, 
-    { str: 'Magenta' }
-  ];
-  myHero = this.heroes[0];
+export class AppComponent implements OnInit {
+  cur: number;
+  title: string;
+  heroes: Hero[];
+  myHero: Hero;
+
+  constructor(private heroService: HeroService) {
+    this.cur = 0; this.title = 'Tour of Heroes';
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+    this.myHero = this.heroes[this.cur];
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   shiftHeroUp() {
     this.heroes.unshift(this.heroes.pop());
     this.myHero = this.heroes[0];
   }
+
   shiftHeroDown() {
     this.heroes.push(this.heroes.shift());
     this.myHero = this.heroes[0];
