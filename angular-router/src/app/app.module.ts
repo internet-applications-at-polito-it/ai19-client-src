603f74981d282d634b8e6485b0834e82b1db7832
Antonio Servetti <antonio.servetti@polito.it>
2019-05-30 14:40:29 +0200

    Added routing and login, register, attendance routes

--- angular-router/src/app/app.module.ts
+++ angular-router/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+import { AttendanceComponent } from './attendance.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'attendance', component: AttendanceComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent, RegisterComponent, AttendanceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-router/src/app/attendance.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance',
+  templateUrl: './attendance.component.html',
+  styleUrls: ['./attendance.component.css']
+})
+export class AttendanceComponent {
+}
